3927c70e78f16c0a927a309c973445ed376ed30a
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-07-11 11:16:31 +0600

    incapsulations.py

--- Tasks/chernovik.py
+++ Tasks/chernovik.py
@@ -1,21 +1,35 @@
-class Taxi:
-    def __init__(self, name, cost, cost_km):
-        self.name = name 
-        self.cost = cost
-        self.cost_km = cost_km
+# class Taxi:
+#     def __init__(self, name, cost, cost_km):
+#         self.name = name 
+#         self.cost = cost
+#         self.cost_km = cost_km
 
-    def get_total_cost(self, km):
-        trip_price = self.cost + self.cost_km * km
-        return f'Такси {self.name}, стоимость такси: {trip_price} сом'
+#     def get_total_cost(self, km):
+#         trip_price = self.cost + self.cost_km * km
+#         return f'Такси {self.name}, стоимость такси: {trip_price} сом'
 
-taxi1 = Taxi("Beka", 80, 10)
-taxi2 = Taxi("sema", 80, 6)
-taxi3 = Taxi("manas", 80, 12)
+# taxi1 = Taxi("Beka", 80, 10)
+# taxi2 = Taxi("sema", 80, 6)
+# taxi3 = Taxi("manas", 80, 12)
 
-print(taxi1.get_total_cost(12))
-print(taxi2.get_total_cost(10))
-print(taxi3.get_total_cost(3))
+# print(taxi1.get_total_cost(12))
+# print(taxi2.get_total_cost(10))
+# print(taxi3.get_total_cost(3))
 
+class A:
+    def method1(self):
+        return "Основной функционал"
+    
+class B(A):
+    def method1(self):
+        return "Основной функционал" 
+
+    def __str__(self):
+        
+
+    
+obj = A()
+print(obj.method1)
 
 
 
---
+++ oop/incapsulation.py
@@ -0,0 +1,53 @@
+"==============Инкапсуляция================"
+# Инкапсуляция - принцип ООП
+# 1. сокрытие данных (ограничение доступа к определенным методам и классам)
+# 2. сбор всех необходимых для класса методов и аттрибутов в "капсулу" (класс)
+
+"==================== Модификаторы доступа к аттрибутам ===================================="
+# 1 public (публичный)
+# 2 protected (защищенный)
+# 3 private (приватный)
+
+# class A:
+#     attr = "публичный"
+#     _attr2 = "защищенный"
+#     __attr3 = "приватный"
+
+
+# A.attr # публичный
+# A._attr2 # защищенный
+# A.__attr3 # AttributeError: type object 'A' has no attribute '__attr3'
+# A._A__attr3 # приватный
+
+"========================== getters/setters ================================="
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+
+    @property # TypeError: 'int' object is not callable
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, new_age):
+        if new_age < 120 and new_age > 0:
+            self.__age = new_age
+        else:
+            raise Exception("age can not be less than 0 or more than 120")
+
+
+obj = Person("Zhiide", 28)
+# print(obj.__age) # AttributeError: 'Person' object has no attribute '__age'
+print(obj.age) # 28
+obj.age = -5 # 
+print(obj.age)
+
+class A:
+    __a = "kksjwssj"
+
+    def get_a(sel):
+        return self.__a
+
+    def set_a(self):
+        return 

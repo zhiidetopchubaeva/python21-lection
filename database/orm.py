5173f5b7a9e576798c91a3a3f203cb3fee474d57
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-07-21 10:20:16 +0600

    orm

---
+++ database/orm.py
@@ -0,0 +1,8 @@
+"================= ORM =========================="
+# ORM - Object Relation Mapping - библиотека, которая позволяет переводить питоновский код в sql запросы и обратно
+
+"ORM for Python"
+# SQLAlchemy
+# Peewee
+
+# Django 
